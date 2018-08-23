88babaab5db75f3181191927ea5b975be3120abd
Philipp von Kirschbaum <info@philipp.vonkirschbaum.com>
2018-08-23 17:57:30 +0200

    Initial Commit

---
+++ app.py
@@ -0,0 +1,11 @@
+mport pandas as pd
+import os
+
+print("CSV-File:")
+print(os.environ.get('CSV'))
+print("PARQUET-File:")
+print(os.environ.get('PARQUET'))
+
+df = pd.read_csv(os.environ.get('CSV'))
+df.to_parquet(os.environ.get('PARQUET'))
+
